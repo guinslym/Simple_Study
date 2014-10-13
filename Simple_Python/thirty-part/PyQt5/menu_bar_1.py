64df1dab00b518794660041ca6c3517b4e1c5022
yafeile <zhuzhulang@126.com>
2014-10-13 11:39:29 +0800

    添加了1个Python27的中文教程

--- Simple_Python/thirty-part/PyQt5/menu_bar_1.py
+++ Simple_Python/thirty-part/PyQt5/menu_bar_1.py
@@ -5,7 +5,6 @@ __author__ = 'tim'
 import sys
 from PyQt5.QtWidgets import QApplication,QMainWindow,QAction,qApp
 from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSignal,pyqtSlot
 
 
 class MainWindow(QMainWindow):
@@ -16,6 +15,7 @@ class MainWindow(QMainWindow):
         exit=QAction(QIcon('48.png'),'Exit',self)
         exit.setShortcut('Ctrl+Q')
         exit.setStatusTip('Exit application')
+        exit.triggered.connect(qApp.exit)
 
         self.statusBar()
         menubar=self.menuBar()
